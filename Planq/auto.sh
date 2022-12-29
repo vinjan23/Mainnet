1096374e39561fe5fb50a8771a3a807408b17b32
vinjan <108977419+vinjan23@users.noreply.github.com>
2022-12-29 15:49:57 +0700

    Create auto.sh

---
+++ Planq/auto.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+
+echo -e "\033[0;35m"
+echo "   #           #  o  #        #          #       #        #        #    ";
+echo "    #         #   #  #  #     #          #      #  #      #  #     #    ";
+echo "     #       #    #  #   #    #          #     #    #     #   #    #    ";
+echo "      #     #     #  #     #  #          #    #  # # #    #     #  #    ";
+echo "       #   #      #  #      # #          #   #        #   #      # #    ";
+echo "         #        #  #        #   # # # #   #          #  #        #    ";
+echo -e "\e[0m"
+
+
+sleep 2
+
+read -p "Enter node moniker: " NODE_MONIKER
+
+
+CHAIN_ID="planq_7070-2"
+CHAIN_DENOM="aplangq"
+BINARY="planqd"
+
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+
+# install go
+if ! [ -x "$(command -v go)" ]; then
+ver="1.19.1"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download and build binaries
+cd || return
+rm -rf planq
+git clone https://github.com/planq-network/planq.git
+cd planq || return
+git fetch
+git checkout v1.0.2
+make install
+mkdir -p $HOME/.planqd/cosmovisor/genesis/bin
+mkdir -p ~/.planqd/cosmovisor/upgrades
+cp ~/go/bin/planqd ~/.planqd/cosmovisor/genesis/bin
+
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
+
+planqd config chain-id $CHAIN_ID
+planqd init $NODE_MONIKER  --chain-id $CHAIN_ID
+planqd config keyring-backend file
+planqd config node tcp://localhost:33657
+
+# download genesis and addrbook
+wget https://raw.githubusercontent.com/planq-network/networks/main/mainnet/genesis.json
+mv genesis.json ~/.planqd/config/
+planqd validate-genesis
+wget -O $HOME/.planqd/config/addrbook.json "https://raw.githubusercontent.com/vinjan23/Mainnet/main/Planq/addrbook.json"
+
+# set peers and seeds
+SEEDS=`curl -sL https://raw.githubusercontent.com/planq-network/networks/main/mainnet/seeds.txt | awk '{print $1}' | paste -s -d, -`
+sed -i.bak -e "s/^seeds =.*/seeds = \"$SEEDS\"/" ~/.planqd/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 100/g' $HOME/.planqd/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/.planqd/config/config.toml
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0aplanq\"/;" ~/.planqd/config/app.toml
+indexer="null" && \
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.planqd/config/config.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.planqd/config/config.toml
+planqd tendermint unsafe-reset-all --home $HOME/.planqd --keep-addr-book
+
+# in case of pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.planqd/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.planqd/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.planqd/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.planqd/config/app.toml
+
+# custom port
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:33658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:33657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:33060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:33656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":33660\"%" $HOME/.planqd/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:33317\"%; s%^address = \":8080\"%address = \":33080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:33090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:33091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:33545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:33546\"%" $HOME/.planqd/config/app.toml
+
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/planqd.service > /dev/null << EOF
+[Unit]
+Description=planq-mainnet node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=$HOME/.planqd"
+Environment="DAEMON_NAME=planqd"
+Environment="UNSAFE_SKIP_BACKUP=true"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl enable planqd && sudo systemctl daemon-reload
+sudo systemctl restart planqd && sudo journalctl -u planqd -f -o cat
+
+echo '=============== SETUP FINISHED ==================='
+
