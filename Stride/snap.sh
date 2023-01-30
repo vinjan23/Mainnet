32d65d894c38ed80b1990880cabf380cdf89e58d
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-01-30 10:40:22 +0700

    Update snap.sh

--- Stride/snap.sh
+++ Stride/snap.sh
@@ -1,15 +1,14 @@
 sudo apt update
-sudo apt install lz4 -y
+sudo apt install snapd -y
+sudo snap install lz4
 
 sudo systemctl stop strided
 
-cp $HOME/.stride/data/priv_validator_state.json $HOME/.stride/priv_validator_state.json.backup
+wget -O stride_2135886.tar.lz4 https://snapshots.polkachu.com/snapshots/stride/stride_2135886.tar.lz4 --inet4-only
+
+lz4 -c -d stride_2135886.tar.lz4  | tar -x -C $HOME/.stride
 strided tendermint unsafe-reset-all --home $HOME/.stride --keep-addr-book
-
-SNAP_NAME=$(curl -s https://snapshots2.nodejumper.io/stride/ | egrep -o ">stride-1.*\.tar.lz4" | tr -d ">")
-curl https://snapshots2.nodejumper.io/stride/${SNAP_NAME} | lz4 -dc - | tar -xf - -C $HOME/.stride
-
-mv $HOME/.stride/priv_validator_state.json.backup $HOME/.stride/data/priv_validator_state.json
+rm -v stride_2135886.tar.lz4
 
 sudo systemctl restart strided
 sudo journalctl -u strided -f --no-hostname -o cat
